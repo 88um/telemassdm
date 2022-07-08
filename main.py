9309c4237121ae7372d5b9e236d88401172a2018
joshua solo <97570113+88um@users.noreply.github.com>
2022-07-07 21:20:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os, random, time
 from telethon.sync import TelegramClient
 from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelRequest
 from telethon.errors.rpcerrorlist import *
-API_ID = 1370903  
-API_HASH = '23c73e7bb6075cd2c909ca51decd7460' 
 
 
 
-def check_files(phone : str) -> tuple:
+
+def check_files(phone : str) -> str:
     global groups #yea
     groups = []
     if not os.path.exists(phone):os.mkdir(phone)
@@ -46,7 +45,7 @@ def remove_group(group : str):
         try:groups.remove(group)
         except:pass
         with open("group_list.txt","r",encoding='utf-8') as f:lines=f.read().splitlines()
-        with open("group_list.txt","w") as f:
+        with open("group_list.txt","w",encoding="utf-8") as f:
             for line in lines:
                 if line!=group:
                     f.write(f"{line}\n")
@@ -63,16 +62,21 @@ def msg_groups(client : TelegramClient, message : str):
             remove_group(group)
         except SlowModeWaitError:
             print(f"\r[{phone}] Slow mode for  -> {group}",end='')
+            #groups.append(group)
     print(f"\r[{phone}] All markets have been messaged! Sleeping 1-2 hours...")
     time.sleep(random.randint(3600,7200))
 
 
 def join_groups(client : TelegramClient):
     phone = client.session.filename.split("/")[0]
+    current_groups = [str(dialog.id) for dialog in client.iter_dialogs()]
     for group in groups:
         try:
+            chat_id= "-100"+str(client.get_entity(group).__getattribute__("id"))
+            if chat_id in current_groups:continue
             client(JoinChannelRequest(group))
-            print(f'\r[{phone}] Joined -> "{group}"',end='')
+            print(f'\r[{phone}] Joined -> "{group} sleep 5 min"',end='')
+            time.sleep(300)
         except ValueError as e:
             remove_group(group)
         except FloodWaitError:
@@ -108,4 +112,8 @@ def main():
         msg_groups(client,message)
 
 
-main()
+if __name__ == "__main__":
+    API_ID = 1370903  
+    API_HASH = '23c73e7bb6075cd2c909ca51decd7460' 
+    main()
+
